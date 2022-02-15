d9ece245094637423e6f3c25f85beb0c20f69a06
dworker <dworker863@gmail.com>
2022-02-15 13:58:54 +0600

    Reviews section slider all screens

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,15 @@
 $(window).on('load', function () {
   $('.owl-carousel').owlCarousel({
-    items: 3,
+    items: 1,
     margin: 40,
     loop: true,
-    responsive: {},
+    responsive: {
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
   });
 });
